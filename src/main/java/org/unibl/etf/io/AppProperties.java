e5e597ee05d83dbeb19a7ba6badddfc55def7d64
Nebojša <ynebojsatolimiry@gmail.com>
2026-06-05 18:55:56 +0200

    add: Properties reader class

---
+++ src/main/java/org/unibl/etf/io/AppProperties.java
@@ -0,0 +1,39 @@
+package org.unibl.etf.io;
+
+import org.unibl.etf.util.AppLogger;
+
+import java.util.Properties;
+
+public final class AppProperties {
+
+    private static final AppProperties INSTANCE = new AppProperties();
+
+    private int terminalCount;
+    private int dockColumns;
+
+    public static AppProperties getInstance() {
+        return INSTANCE;
+    }
+
+    private AppProperties() {
+        try(var inputStream = AppProperties.class.getResourceAsStream("/properties.yaml")) {
+            var properties = new Properties();
+
+            properties.load(inputStream);
+
+            terminalCount = Integer.parseInt(properties.getProperty("application.terminal.count"));
+            dockColumns = Integer.parseInt(properties.getProperty("application.terminal.dock.columns"));
+
+        } catch (Exception e) {
+            AppLogger.getInstance().severe("Failed to load properties.", e);
+        }
+    }
+
+    public int getTerminalCount() {
+        return terminalCount;
+    }
+
+    public int getDockColumns() {
+        return dockColumns;
+    }
+}
